e76de7c473aa1dc63dcb68f6cdf7028eedf18bc0
kyacine7 <44146681+kyacine7@users.noreply.github.com>
2018-11-12 23:26:47 +0100

    Update formulaire.js

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,62 +1,21 @@
 
 function validation() {
-    if (document.getElementById("nom").value == "") {
-        document.getElementById("erreur").innerHTML = "La saisie du nom est obligatoire"
 
-        document.getElementById("resultat").style.display = "none"
+    var minlength=5;
+    var nom = document.getElementById("nom");
+    var prenom = document.getElementById("prenom");
+    var dn = document.getElementById("dn");
+    var adresse = document.getElementById("adresse");
+    var mail = document.getElementById("mail");
+
+
+    if( nom.value.length >= minlength && prenom.value.length >= minlength && dn.value.length >= minlength 
+        && adresse.value.length >= minlength && mail.value.length >= minlength){
+            document.getElementById('resultat').style.display='block';
+            document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value;
     }
-    else if (document.getElementById("nom").value.length < 5) {
-        document.getElementById("erreur").innerHTML = "le nom doit contenir au moins 5caractères "
+    else{
+            document.getElementById('error').style.display='block';
+            document.getElementById("error").innerHTML = "Les champs doivent avoir minimum "+minlength+" caractéres";
     }
-
-
-
-    else if (document.getElementById("prénom").value == "") {
-        document.getElementById("erreur").innerHTML = "La saisie du prénom est obligatoire"
     }
-    else if (document.getElementById("prénom").value.length < 5) {
-        document.getElementById("erreur").innerHTML = "le prénom doit contenir au moins 5caractères "
-        document.getElementById("resultat").style.display = "none"
-    }
-
-
-    else if (document.getElementById("date").value == "") {
-        document.getElementById("erreur").innerHTML = "La séléction de votre date de naissance est obligatoire"
-        document.getElementById("resultat").style.display = "none"
-    }
-
-
-
-
-    else if (document.getElementById("adresse").value == "") {
-        document.getElementById("erreur").innerHTML = "La saisie de votre adresse est obligatoire"
-        document.getElementById("resultat").style.display = "none"
-    }
-    else if (document.getElementById("adresse").value.length < 5) {
-        document.getElementById("erreur").innerHTML = "l'adresse doit contenir au moins 5caractères "
-        document.getElementById("resultat").style.display = "none"
-    }
-
-
-
-    else if (document.getElementById("mail").value == "") {
-        document.getElementById("erreur").innerHTML = "La saisie du mail est obligatoire"
-        document.getElementById("resultat").style.display = "none"
-
-    }
-    else if (document.getElementById("mail").value.length < 5) {
-        document.getElementById("erreur").innerHTML = "la saisie du mail est obligatoire "
-        document.getElementById("resultat").style.display = "none"
-    }
-
-    else {
-        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prénom").value;
-        document.getElementById("erreur").style.display = "none"
-        document.getElementById("resultat").style.display = "block"
-
-    }
-
-}
-
-
-
