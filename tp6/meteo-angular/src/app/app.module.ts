d86824656b224a4e10f66afb5574967180d4821b
kyacine7 <44146681+kyacine7@users.noreply.github.com>
2019-02-27 19:59:42 +0100

    1

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
 
-@NgModule({
+/*@NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+*/
+import { RouterModule, Routes } from '@angular/router';
+
+
+//ajouter par le prof
+import { FormsModule } from '@angular/forms'; // <--  Ajouter la référence ici
+
+//import { AppComponent } from './app.component';
+//import { MeteoComponent } from './meteo/meteo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeteoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule //<-- Ajouter la référence ici aussi
   ],
   providers: [],
   bootstrap: [AppComponent]
